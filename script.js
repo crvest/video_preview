0aca337f73fac58250febea4efa32e7fff7febb2
crvest <chad.vest@gmail.com>
2022-03-28 16:17:27 -0400

    video_preview complete

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+// on mouse over play video
+function playVid (element) {
+    element.play();
+}
+
+// on mouse leave pause video
+function pauseVid(element) {
+    element.pause();
+}
